66ac7bdd8e8008f1e8a3b6815f9dd5e871bc0025
Iti Tyagi <intechworx@gmail.com>
2018-01-12 18:29:22 +0530

    Add files via upload

---
+++ Readify/IRedPill.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace TestReadifyChallenge
+{
+    [ServiceContract]
+    public interface IRedPill
+    {
+        [System.ServiceModel.OperationContractAttribute]
+        System.Guid WhatIsYourToken();
+
+        [System.ServiceModel.OperationContractAttribute]
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentOutOfRangeException))]
+        long FibonacciNumber(long n);
+
+        [System.ServiceModel.OperationContractAttribute]
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentOutOfRangeException))]
+        TriangleType WhatShapeIsThis(int a, int b, int c);
+
+        [System.ServiceModel.OperationContractAttribute]
+        [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentOutOfRangeException))]
+        string ReverseWords(string s);
+
+    }
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
+    [System.Runtime.Serialization.DataContractAttribute]
+    public enum TriangleType : int
+    {
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Error = 0,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Equilateral = 1,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Isosceles = 2,
+
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Scalene = 3,
+    }
+
+}
